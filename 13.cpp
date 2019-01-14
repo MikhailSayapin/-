ef632758e56990b9222e383d82366b28baf2a1ac
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2019-01-15 00:55:38 +0300

    Create 13.cpp

---
+++ 13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename Iterator, typename T>
+
+/* 
+* В функции первый итератор будет налажен на i-тое число, 
+* а второе будет пробегать все элементы коллекции, кроме
+* i - го элемента, суммировать их и сравнивать с числом с
+*/
+
+pair<bool, pair<Iterator, Iterator>>find_two_elements_with_sum(Iterator first, Iterator last, T c)
+{
+	for()
+}
