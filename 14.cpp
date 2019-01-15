df56bed2248ba59585ecf406932cd01545fa2b98
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2019-01-15 20:57:02 +0300

    Create 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename Iterator1, typename Iterator2>
+Iterator2 merge(Iterator1 &first1, Iterator1 &last1, Iterator1 &first2, Iterator1 &last2, Iterator2 &output)
+{
+	Iterator2 temp;
+	last1 = first2;
+}
